ee2de2de49fa2b97e8aa30cc7f2a76f0cac5ede7
Aman Prakash <114851919+AmanPrakash00@users.noreply.github.com>
2022-12-24 07:54:14 +0530

    Add files via upload

---
+++ Print alternate elements of an array.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+// ar[] is the array 
+// n is the number of elements in array.
+void print(int ar[], int n)
+{
+    
+    // code here
+    for(int i = 0 ; i < n ; i++){
+        cout<< ar[i]<<" ";
+        i++;
+    }
+    
+    
+    
+}
+
+
+// } Driver Code Ends
