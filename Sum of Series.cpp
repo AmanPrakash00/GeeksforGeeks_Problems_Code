313372f2efb33077a700fc5e7f37f836f0500829
Aman Prakash <114851919+AmanPrakash00@users.noreply.github.com>
2022-12-25 10:52:08 +0530

    Add files via upload

---
+++ Sum of Series.cpp
@@ -0,0 +1,15 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// function to return sum of  1, 2, ... n
+	long long seriesSum(int n) {
+	  
+	   return (long)n*(n+1)/2;
+	}
+};
