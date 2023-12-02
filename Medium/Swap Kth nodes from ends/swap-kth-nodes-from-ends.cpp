4e8f3e68d0ddf0af65d416d8f0ca8069d4cb0a30
Aman Prakash <114851919+AmanPrakash00@users.noreply.github.com>
2023-12-02 19:38:04 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Swap Kth nodes from ends/swap-kth-nodes-from-ends.cpp
@@ -0,0 +1,132 @@
+//{ Driver Code Starts
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+
+Node *swapkthnode(Node* head, int num, int K);
+
+void addressstore(Node **arr, Node* head)
+{
+    Node* temp = head;
+    int i = 0;
+    while(temp){
+        arr[i] = temp;
+        i++;
+        temp = temp->next;
+    }
+}
+
+bool check(Node ** before, Node **after, int num, int K)
+{
+    if(K > num)
+        return 1;
+        
+    bool f=true;
+    
+    for(int i=0; i<num; i++){
+        if((i==K-1) || (i==num - K)){
+            if(!((before[K-1] == after[num - K]) && (before[num-K] == after[K-1]))) f=0;
+        }
+        else{
+            if(before[i] != after[i]) f=0;
+        }
+    }
+    
+    return f;
+}
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--){
+        int num, K , firstdata;
+        cin>>num>>K;
+        int temp;
+        cin>>firstdata;
+        Node* head = new Node(firstdata);
+        Node* tail = head;
+        for(int i = 0; i<num - 1; i++){
+            cin>>temp;
+            tail->next = new Node(temp);
+            tail = tail->next;
+        }
+        
+        Node *before[num];
+        addressstore(before, head);
+        
+        head = swapkthnode(head, num, K);
+        
+        Node *after[num];
+        addressstore(after, head);
+        
+        if(check(before, after, num, K))
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+/* Linked List Node structure
+   struct Node  {
+     int data;
+     Node *next;
+     Node(int x) {
+        data = x;
+        next = NULL;
+  }
+  }
+*/
+
+//Function to swap Kth node from beginning and end in a linked list.
+Node *swapkthnode(Node* head, int num, int K)
+{
+    // Your Code here
+     if(K>num) return head;
+   Node *first=head,*second=head,*temp;
+   Node *fprev=NULL,*lprev=NULL;
+   for(int i=0;i<K-1;i++){
+       fprev=first;
+       first=first->next;
+   } 
+   for(int i=0;i<num-K;i++){
+       lprev=second;
+       second=second->next;
+   } 
+   if(K==1){
+       lprev->next=first;
+       second->next=first->next;
+       first->next=NULL;
+       return second;
+   }
+   if(K==num){
+       fprev->next=second;
+       first->next=second->next;
+       second->next=NULL;
+       return first;
+   }
+   else{
+       fprev->next=second;
+       lprev->next=first;
+       temp=first->next;
+       first->next=second->next;
+       second->next=temp;
+       return head;
+   }
+}
